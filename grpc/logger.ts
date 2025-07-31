8e785351614fdef2473bf33c10b8e25bb1a935ea
oreki <saarthak22421@iiitd.ac.in>
2025-08-01 01:13:39 +0530

    fix

--- grpc/logger.ts
+++
@@ -1,7 +0,0 @@
-// Simple timestamped logger utility for the gRPC client & examples
-
-export function log(...args: unknown[]): void {
-  const timestamp = new Date().toISOString();
-  // eslint-disable-next-line no-console
-  console.log(`[${timestamp}]`, ...args);
-} 
